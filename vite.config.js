bbd13768bc6e747bf4d21c5961c70c2b8ed01f51
Dmytro <tom.service1030@gmail.com>
2025-03-03 18:49:53 +0200

    update vite config

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,8 @@ export default defineConfig({
     server: {
         host: '0.0.0.0', // Allows external connections
         port: 5173, // Default port; change if needed
-    },
+        fs: {
+            allow: ['/home/azureuser/cheapernights_frontend_new']
+        },
+    }
 });
